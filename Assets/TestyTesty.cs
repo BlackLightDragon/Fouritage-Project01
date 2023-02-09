2c33404a38f9e1e544347dc67fbe25fd672c08a3
Kayak0o <nexus.grosche@gmail.com>
2023-02-09 22:37:31 +0100

    test

--- Assets/TestyTesty.cs
+++ Assets/TestyTesty.cs
@@ -7,7 +7,7 @@ public class TestyTesty : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("hit444");
+       Debug.Log("hit666");
     }
 
     // Update is called once per frame
