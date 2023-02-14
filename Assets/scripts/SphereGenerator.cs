42f998ad9865abc687a0391f41161825bf6f5758
BlackLightDragon <108180227+BlackLightDragon@users.noreply.github.com>
2023-02-14 21:07:37 +0100

    SphereGenerator v0.1

--- Assets/scripts/SphereGenerator.cs
+++ Assets/scripts/SphereGenerator.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(MeshFilter))]
+public class SphereGenerator : MonoBehaviour{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triagles;
+
+    void Start(){
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void CreateShape(){
+
     }
+
+    
 }
--- Assets/scripts/TestyTesty.cs
+++ Assets/scripts/TestyTesty.cs
@@ -7,7 +7,14 @@ public class TestyTesty : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("hit666");
+        var list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        foreach (var i in list)
+        {
+            Debug.Log(i);
+        }
+        Debug.Log("hit666");
     }
 
     // Update is called once per frame
